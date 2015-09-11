306f1a17c65c328d3349328b3758ca36b97bfc9c
Rudie Dirkx <github@hotblocks.nl>
2015-09-11 20:20:37 +0200

    #8 mail tpl+

--- tpl.new-posts.php
+++ tpl.new-posts.php
@@ -1,10 +1,10 @@
 
 <h2><a href="<?= h($blog->url) ?>"><?= $blog->title ?></a> (<?= count($posts) ?>)</h2>
 
-<table border="1" cellpadding="10" cellspacing="0">
+<table border="1" cellpadding="10" cellspacing="0" width="100%">
 	<? foreach ($posts as $post): ?>
 		<tr>
-			<td>
+			<td width="200">
 				<? if ($post->image): ?>
 					<a href="<?= h($post->url) ?>">
 						<img src="<?= h($post->image) ?>" alt="<?= h($post->title) ?>" style="max-width: 200px; max-height: 200px" />
